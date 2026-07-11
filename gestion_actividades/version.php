40772264d887aa537840d2fe89f3713f9c04fb82
martinruizjulio-del <martinruizjulio@gmail.com>
2026-07-11 11:47:01 +0200

    Subir versión 1.5.0-alpha del portafolio con índice y anexos

--- gestion_actividades/version.php
+++ gestion_actividades/version.php
@@ -6,7 +6,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_gestion_actividades';
-$plugin->version = 2026071076;
+$plugin->version = 2026071077;
 $plugin->requires  = 2022041900; // Moodle 4.0+.
 $plugin->maturity  = MATURITY_ALPHA;
-$plugin->release = '1.4.9-alpha';
+$plugin->release = '1.5.0-alpha';
