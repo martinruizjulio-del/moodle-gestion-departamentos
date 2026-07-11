8971faa25728a31c2a7cb8171bd2835d637823e0
martinruizjulio-del <martinruizjulio@gmail.com>
2026-07-11 10:42:08 +0200

    Añadir edición de portada del portafolio

---
+++ gestion_actividades/portfolio_cover_template.php
@@ -0,0 +1,36 @@
+<?php
+require_once(__DIR__ . '/../../config.php');
+
+use local_gestion_actividades\local\portfolio_pdf;
+
+require_login();
+$context = context_system::instance();
+require_capability('local/gestion_actividades:manage', $context);
+
+if (data_submitted() && confirm_sesskey()) {
+    $html = required_param('coverhtml', PARAM_RAW);
+    portfolio_pdf::save_cover_template($html);
+    redirect(new moodle_url('/local/gestion_actividades/portfolio_cover_template.php'), 'Portada guardada correctamente.');
+}
+
+$PAGE->set_context($context);
+$PAGE->set_url(new moodle_url('/local/gestion_actividades/portfolio_cover_template.php'));
+$PAGE->set_title('Portada del portafolio');
+$PAGE->set_heading('Portada del portafolio');
+
+echo $OUTPUT->header();
+echo $OUTPUT->heading('Portada editable del portafolio');
+echo html_writer::tag('p', 'Esta portada se usará en todos los PDF de portafolio. Puedes editar el texto y usar las variables automáticas.', ['class' => 'alert alert-info']);
+
+echo html_writer::start_tag('form', ['method' => 'post']);
+echo html_writer::empty_tag('input', ['type' => 'hidden', 'name' => 'sesskey', 'value' => sesskey()]);
+echo html_writer::tag('textarea', s(portfolio_pdf::get_cover_template()), ['name' => 'coverhtml', 'class' => 'form-control', 'rows' => 14]);
+echo html_writer::empty_tag('input', ['type' => 'submit', 'value' => 'Guardar portada', 'class' => 'btn btn-primary mt-3']);
+echo ' ';
+echo html_writer::link(new moodle_url('/local/gestion_actividades/portfolio_admin.php'), 'Volver al portafolio gestor', ['class' => 'btn btn-secondary mt-3']);
+echo html_writer::end_tag('form');
+
+echo html_writer::tag('h3', 'Variables disponibles', ['class' => 'mt-4']);
+echo html_writer::tag('pre', "{alumno}\n{curso}\n{horas_tipo_a}\n{horas_tipo_b}\n{horas_total}\n{fecha_emision}");
+
+echo $OUTPUT->footer();
