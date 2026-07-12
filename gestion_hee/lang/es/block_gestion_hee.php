67a2cf0815996267fe3f4a7148c2506c3b42b5e5
martinruizjulio-del <martinruizjulio@gmail.com>
2026-07-13 00:29:11 +0200

    Update block_gestion_hee.php

--- gestion_hee/lang/es/block_gestion_hee.php
+++ gestion_hee/lang/es/block_gestion_hee.php
@@ -1,1 +1,15 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
 
+$string['pluginname'] = 'Gestión HEE - Alumno';
+$string['gestion_hee:addinstance'] = 'Añadir bloque Gestión HEE - Alumno';
+$string['gestion_hee:myaddinstance'] = 'Añadir bloque Gestión HEE - Alumno al área personal';
+$string['title'] = 'Gestión HEE';
+$string['typeahours'] = 'Horas Tipo A completadas';
+$string['typebhours'] = 'Horas Tipo B completadas';
+$string['remaininghours'] = 'Horas restantes hasta 54';
+$string['totalhours'] = 'Total reconocido';
+$string['mycertificates'] = 'Mis certificados';
+$string['myportfolio'] = 'Mi portafolio';
+$string['nohoursyet'] = 'Aún no hay horas reconocidas.';
+$string['uploadtypeb'] = 'Subir certificado Tipo B';
