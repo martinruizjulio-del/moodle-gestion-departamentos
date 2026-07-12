6c635a0fecd194f10d9cd1dac314ac1d6efdd66e
martinruizjulio-del <martinruizjulio@gmail.com>
2026-07-13 00:31:45 +0200

    Add files via upload

---
+++ gestion_hee/version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'block_gestion_hee';
+$plugin->version = 2026071124;
+$plugin->requires = 2022041900;
+$plugin->release = '1.0.5-alpha';
+$plugin->maturity = MATURITY_ALPHA;
