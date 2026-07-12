638e77ba05ea5afe4304e1f68adc6ed8bf49e6f7
martinruizjulio-del <martinruizjulio@gmail.com>
2026-07-13 00:26:44 +0200

    Update access.php

--- gestion_hee/db/access.php
+++ gestion_hee/db/access.php
@@ -1,1 +1,23 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
 
+$capabilities = [
+    'block/gestion_hee:addinstance' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => [
+            'manager' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+        ],
+        'clonepermissionsfrom' => 'moodle/site:manageblocks',
+    ],
+
+    'block/gestion_hee:myaddinstance' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [
+            'user' => CAP_ALLOW,
+        ],
+        'clonepermissionsfrom' => 'moodle/my:manageblocks',
+    ],
+];
