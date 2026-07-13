44627e6ff05785779bbde334082ab59baf348f48
martinruizjulio-del <martinruizjulio@gmail.com>
2026-07-13 22:48:09 +0200

    Update block_gestion_hee to v1.0.8-alpha

--- gestion_hee/version.php
+++ gestion_hee/version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_gestion_hee';
-$plugin->version = 2026071124;
+$plugin->version = 2026071127;
 $plugin->requires = 2022041900;
-$plugin->release = '1.0.5-alpha';
+$plugin->release = '1.0.8-alpha';
 $plugin->maturity = MATURITY_ALPHA;
