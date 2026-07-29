9c62d267b04e2f0d9d8ce46fd547b0da57f90ad2
martinruizjulio-del <martinruizjulio@gmail.com>
2026-07-29 12:52:09 +0200

    Set Gestión HEE block release to 1.0.16-alpha

--- gestion_hee/version.php
+++ gestion_hee/version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_gestion_hee';
-$plugin->version = 2026071127;
+$plugin->version = 2026071416;
 $plugin->requires = 2022041900;
-$plugin->release = '1.0.8-alpha';
+$plugin->release = '1.0.16-alpha';
 $plugin->maturity = MATURITY_ALPHA;
